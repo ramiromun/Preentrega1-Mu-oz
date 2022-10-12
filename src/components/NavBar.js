df3432d04b5279e4bf7ecf2ba871697c6d669560
ramiromun <ramiromun152@gmail.com>
2022-10-12 20:45:48 -0300

    Prentrega1-Muñoz

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+
+function NavBar () {
+    return (
+        
+        <><div className="titulo"> tienda </div><div>
+            <a href="#">remeras</a>
+            <a href="#">buzos</a>
+            <a href="#">pantalosnes</a>
+        </div></>
+    )
+}
+  
